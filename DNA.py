3dc291d97e816131245c527ccc5ae57d53b2a776
Adam Howard <awhoward112@gmail.com>
2015-04-24 20:37:43 -0400

    initial commit
    committing file for DNA functions

---
+++ DNA.py
@@ -0,0 +1,33 @@
+def CountNucleotides(input):
+    a = input.count('A')
+    c = input.count('C')
+    g = input.count('G')
+    t = input.count('T')
+
+    return "a: " + str(a) + " c: " + str(c) + " g: " + str(g) + " t: " + str(t)
+
+def PatternCount(text, pattern):
+    count = 0
+    indexes = ""
+
+    for i in range(len(text)):
+        if text[i:i+len(pattern)] == pattern:
+            print(i)
+            indexes += str(i) + " "
+            count += 1
+
+    return indexes
+
+def FrequentWords(text, k):
+    frequentPatterns = []
+    count = []
+
+    for i in range(len(text) - k):
+        pattern = text[i:i+k]
+        count.append(PatternCount(text, pattern))
+    maxCount = max(count)
+    for j in range(len(text) - k):
+        if count[j] == maxCount:
+            frequentPatterns.append(text[j:j+k])
+
+    return list(set(frequentPatterns))
